bd5c9798e7c769dece6bfd38c03db01fabbb059c
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-11 17:06:47 -0400

    Time: 1 ms (74.6%), Space: 41.3 MB (25.63%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.java
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.java
@@ -1,4 +1,8 @@
+//Orignally I thought I was going to need to find all possible permutations for the number I was given but I ended up finding a much more efficient way
+//By instead finding the frequency in which all the numbers popped up in the given number I could compare that to all of the powers of two within 10^9.
+
 class Solution {
+    //helper fuction that finds the frequency of each number in the number it is given.
     private int[] numberFrequency(int num) {
         String s = String.valueOf(num);
         int[] frequency = new int[10];
@@ -20,7 +24,8 @@ class Solution {
 
         int[] frequency = numberFrequency(n);
 
-
+        //here I create a frequency arrary for each power of two and then check it agaisnt the one I made for the number I was given.
+        //the inside loop also ends early if a the frequency doesn't match for the current digit.
         for (int i = 1; i < 31; i++) {
             boolean isEqual = true;
             int[] power = numberFrequency((int)Math.pow(2, i));
@@ -30,6 +35,7 @@ class Solution {
                     break;
                 }
             }
+            //just to make sure I don't have make extra checks I check here to see if isEqual is still true and if it is we can quit searching and return true.
             if (isEqual) {
                 return true;
             }
