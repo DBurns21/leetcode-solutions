d55582af051bb138c3c2d614176450cef5ade907
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-12 15:46:31 -0400

    Time: 83 ms (65.43%), Space: 45.2 MB (37.23%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.java
@@ -13,22 +13,24 @@ class Solution {
             return memo[index][total];
         }
 
+        //you will always want to compute other possible option for if you did skip the current power even if it is less than the total
         int ifSkipped = dp(total, powers, index - 1, memo) % MOD;
         int notSkipped = 0;
-
+        //in the event that we do actually skip this power then only 0 will be added to the return value causing no harm but if we don't skip we check what would happen
         if (powers[index] <= total) { 
             notSkipped = dp(total - powers[index], powers, index - 1, memo) % MOD;
         }
+        //a little lazy but we can return the current memo and also set it equal to ifSkipped and notSkipped in the same line
         return memo[index][total] = ifSkipped + notSkipped;
     }
 
     public int numberOfWays(int n, int x) {
-        List<Integer> answers = new ArrayList<>();
         int[] powers = new int[n+1];
         for (int i = 1; i <= n; ++i) {
             powers[i] = (int)Math.pow(i, x);
         }
 
+        // is used to store results so that if we come across a combination we have found before we can skip the process of finding it again and just get the result
         int[][] memo = new int[powers.length][n + 1];
         for (int i = 0; i < powers.length; ++i) {
             for (int j = 0; j <= n; ++j) {
