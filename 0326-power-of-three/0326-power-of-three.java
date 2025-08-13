f36ea322b06e585377951c6d04ae36403fc9d049
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-13 14:59:23 -0400

    Time: 8 ms (84.41%), Space: 44.2 MB (51.08%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,7 +1,11 @@
 class Solution {
+    //since we are using int it only goes up to 32 bits
+    //this means that the largest base power of 3 we can get is 1162261467
     private final int largestPower = 1162261467;
 
     public boolean isPowerOfThree(int n) {
+        //since any base of 3 is divisible by only any of base power of 3 we just
+        //need to check if we divide n by the largest base 3 power we can get and see if their is no remainder
         return (n > 0 && largestPower % n == 0);
     }
 }
