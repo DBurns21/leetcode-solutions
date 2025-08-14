f41b131f7d7286cc2942c29910fca4d5384d52a8
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-14 09:22:26 -0400

    Time: 3 ms (51.2%), Space: 42.3 MB (49.2%) - LeetHub

--- 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/2346-largest-3-same-digit-number-in-string.java
@@ -1,9 +1,10 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        if (num.length() < 3) {
-            return "";
-        }
+        //origanally I checked if the string was less than 3 but in the constraints the shortest length of n is 3 so I removed it
         String substring = "";
+
+        //I go through all but the last 2 digits and check if the current digit is equal to the two after it and if it is I check if the substring has been updated
+        //if it hasn't been updated I update the current substring into it but if it has I check if it has a larger value than the substring.
         for (int i = 0; i < num.length() - 2; ++i) {
             if (num.charAt(i) == num.charAt(i+1) && num.charAt(i) == num.charAt(i+2)){
                 if (substring.length() == 0 || (Integer.parseInt(substring) < Integer.parseInt(num.substring(i, i+3)))) {
