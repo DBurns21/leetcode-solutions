8156f41bf86e5e95ff9b4d65179ae39bb39edfde
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-15 14:48:05 -0400

    Time: 0 ms (100%), Space: 41.2 MB (24.63%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n > 4) {
+            if (n % 4 != 0) {
+                return false;
+            }
+
+            n /= 4;
+        }
+
+        return n == 1 || n == 4;
+    }
+}
