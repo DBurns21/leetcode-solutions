8811658c35261f1aeb3d4c1a607b6af935312dcc
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-19 15:30:03 -0400

    Time: 6 ms (26.61%), Space: 44.3 MB (35.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -5,7 +5,7 @@ class Solution {
         }
         String str = String.valueOf(x);
 
-        for (int i = 0; i <= Math.floor(str.length() / 2); i++) {
+        for (int i = 0; i <= str.length() / 2; i++) {
             if (str.charAt(i) != str.charAt(str.length () - 1 - i)) {
                 return false;
             }
