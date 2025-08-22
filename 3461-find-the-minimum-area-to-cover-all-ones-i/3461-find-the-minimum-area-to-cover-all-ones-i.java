277ba52b45968a23fc7fc828da3776bb7228b37a
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-22 13:36:07 -0400

    Time: 1 ms (100%), Space: 197.4 MB (14.63%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.java
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.java
@@ -1,11 +1,53 @@
 class Solution {
     public int minimumArea(int[][] grid) {
-        //left to right
-        int minI = grid.length;
+        int m = grid.length;
+        int n = grid[0].length;
+        int minI = m;
         int maxI = 0;
-        int minJ = grid[0].length;
+        int minJ = n;
         int maxJ = 0;
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    minI = i;
+                    i = m;
+                    break;
+                }
+            }
+        }
+
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1;j >= 0; j--) {
+                if (grid[i][j] == 1) {
+                    maxI = i;
+                    i = -1;
+                    break;
+                }
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == 1) {
+                    minJ = j;
+                    j = n;
+                    break;
+                }
+            }
+        }
+
+        for (int j = n-1; j >= 0; j--) {
+            for (int i = m-1; i >= 0; i--) {
+                if (grid[i][j] == 1) {
+                    maxJ = j;
+                    j = -1;
+                    break;
+                }
+            }
+        }
+
+        /*
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[0].length; ++j) {
                 if (grid[i][j] == 1){
@@ -25,6 +67,7 @@ class Solution {
                 }
             }
         }
+        */
         return (maxJ - minJ + 1) * (maxI - minI + 1);
     }
 }
