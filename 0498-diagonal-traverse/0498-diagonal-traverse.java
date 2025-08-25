d04c4bbb2ba6d476673f9093bb1e6ac86c44451c
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-25 09:53:39 -0400

    Time: 155 ms (5%), Space: 47.6 MB (19.76%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int i = 0;
+        int j = 0;
+        Boolean up = true;
+        int location = 0;
+        int[] diagonal = new int[m*n];
+        
+        while (i < m && j < n) {
+            diagonal[location] = mat[i][j];
+            location++;
+
+            if (up) {
+                if( i == 0) {
+                    up = false;
+                    if (j == n-1) {
+                        i++;
+                    }
+                    else {
+                        j++;
+                    }
+                }
+                else if (j == n-1) {
+                    up = false;
+                    i++;
+                }
+                else {
+                    i--;
+                    j++;
+                }
+                System.out.println("i: " + i + "  j: " + j);
+            }
+            else {
+                if (j == 0) {
+                    up = true;
+                    if (i == m-1) {
+                        j++;
+                    }
+                    else {
+                        i++;
+                    }
+                }
+                else if (i == m-1) {
+                    up = true;
+                    j++;
+                }
+                else {
+                    i++;
+                    j--;
+                }
+                System.out.println("i: " + i + "  j: " + j);
+            }
+        }
+        return diagonal;
+    }
+}
