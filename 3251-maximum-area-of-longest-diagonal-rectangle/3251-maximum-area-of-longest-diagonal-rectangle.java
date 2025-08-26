ba34d4c3b1abb9604c9c8484780fb875498d1703
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-26 08:47:40 -0400

    Time: 1 ms (96%), Space: 44.2 MB (90.15%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.java
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.java
@@ -1,19 +1,23 @@
 class Solution {
     public int areaOfMaxDiagonal(int[][] dimensions) {
-        int largest = dimensions[0][0] * dimensions[0][0] + dimensions[0][1] * dimensions[0][1];
-        int area = dimensions[0][0] * dimensions[0][1];
-        System.out.println( Math.sqrt(dimensions[0][0] * dimensions[0][0] + dimensions[0][1] * dimensions[0][1]));
+        int largest = 0;
+        int area = 0;
 
-        for (int i = 1; i < dimensions.length; ++i) {
-            int check = dimensions[i][0] * dimensions[i][0] + dimensions[i][1] * dimensions[i][1];
-            System.out.println(check);
+        for (int i = 0; i < dimensions.length; ++i) {
+            //changed code a little bit for better readability
+            //now instead of dimensions[i][0 or 1] I have it set to a or b 
+            int a = dimensions[i][0];
+            int b = dimensions[i][1];
+            //we also don't need to find the square root so we just skip that part 
+            int check = a * a + b * b;
 
             if (check > largest) {
                 largest = check;
-                area = dimensions[i][0] * dimensions[i][1];
+                area = a * b;
             }
+            //in the case that more than one have the same size we then want to see which has the larger area which is what made me fail a few test orignally
             else if (check == largest) {
-                int tempArea = dimensions[i][0] * dimensions[i][1];
+                int tempArea = a * b;
                 if (tempArea > area) {
                     area = tempArea;
                 }
