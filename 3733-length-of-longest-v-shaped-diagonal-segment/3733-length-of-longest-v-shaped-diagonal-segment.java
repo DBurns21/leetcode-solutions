4b97f6a9819f8feb6f4abbab57bd5c813193c852
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-08-27 12:59:47 -0400

    Time: 940 ms (59.21%), Space: 78.6 MB (56.58%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.java
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.java
@@ -1,15 +1,12 @@
 class Solution {
     private int search(int i, int j, int[] dir, int[][] grid, boolean hasTurned, boolean wasTwo, int num) {
         if (i >= grid.length || j >= grid[0].length || i < 0 || j < 0) {
-            //System.out.println("failed one: " + num + "  I: " + i + "  J: " + j);
             return 0;
         }
         int current = grid[i][j];
         if ((current != 0 && current != 2) || (wasTwo && current == 2) || (!wasTwo && current == 0)) {
-            //System.out.println("failed two: " + num + "  I: " + i + "  J: " + j);
             return 0;
         }
-        //System.out.println("I: " + i + "  J: " + j + "  dir: " + dir[0] + ", " + dir[1] + "  Num: " + num);//+ "  Current: " + grid[i][j] + "  wasTwo: " + wasTwo);
 
         if (current == 2) {
             wasTwo = true;
@@ -39,24 +36,19 @@ class Solution {
         int four = 1;
 
         if (i != m-1 && j != n-1) {
-           // System.out.println("one: " + i + ", " + j);
             one = 1 + search(i + dir[0][0], j + dir[0][1], dir[0], grid, false, false, 1);
         }
         if (i != m-1 && j != 0) {
-            //System.out.println("two: " + i + ", " + j);
             two = 1 + search(i + dir[1][0], j + dir[1][1], dir[1], grid, false, false, 2);
         }
         if (i != 0 && j != 0) {
-            //System.out.println("three: " + i + ", " + j);
             three = 1 + search(i + dir[2][0], j + dir[2][1], dir[2], grid, false, false, 3);
         }
         if (i != 0 && j != n-1) {
-            //System.out.println("four: " + i + ", " + j);
             four = 1 + search(i + dir[3][0], j + dir[3][1], dir[3], grid, false, false, 4);
         }
         length = Math.max(length, Math.max(one, Math.max(two, Math.max(three, four)))); 
         
-        //System.out.println(length);
         return length;
     }
 
