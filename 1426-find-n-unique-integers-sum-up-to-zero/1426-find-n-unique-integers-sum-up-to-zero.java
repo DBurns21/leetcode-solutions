dd78795b0aadc4f32413b4c62d288249728a7f6c
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-09-07 16:13:18 -0400

    Time: 0 ms (100%), Space: 41.6 MB (70.86%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.java
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.java
@@ -2,9 +2,11 @@ class Solution {
     public int[] sumZero(int n) {
         int[] uniqueZero = new int[n];
 
-        if (n % 2 == 1) {
-            uniqueZero[n/2] = 0;
-        }
+        //due to the fact that java makes every spot in an array a 0 when created we don't actually need to
+        //check if n is evven or odd
+        //if (n % 2 == 1) {
+        //    uniqueZero[n/2] = 0;
+        //}
 
         for (int i = 0; i < n/2; ++i) {
             int current  = n/2 - i;
