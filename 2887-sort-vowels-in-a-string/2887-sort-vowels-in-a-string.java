92d06eab254b190f9364649bd7fd408e940c9b88
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-09-11 15:17:08 -0400

    Time: 952 ms (5.25%), Space: 47.5 MB (48.98%) - LeetHub

---
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -0,0 +1,105 @@
+class Solution {
+    public String sortVowels(String s) { 
+        //AEIOUaeiou
+        int[] freq = new int[10];
+        
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            char lowercase = Character.toLowerCase(c);
+
+            if (lowercase == 'a' || lowercase == 'e' || lowercase == 'i' || lowercase == 'o' || lowercase == 'u') {
+               
+                if (c == 'A') {
+                    freq[0]++;
+                }
+                else if (c == 'E') {
+                    freq[1]++;
+                }
+                else if (c == 'I') {
+                    freq[2]++;
+                }
+                else if (c == 'O') {
+                    freq[3]++;
+                }
+                else if (c == 'U') {
+                    freq[4]++;
+                }
+                else if (c == 'a') {
+                    freq[5]++;
+                }
+                else if (c == 'e') {
+                    freq[6]++;
+                }
+                else if (c == 'i') {
+                    freq[7]++;
+                }
+                else if (c == 'o') {
+                    freq[8]++;
+                }
+                else {
+                    freq[9]++;
+                }
+            }
+        }
+        
+        
+        for (int i = 0; i < freq.length; ++i) {
+            System.out.println(freq[i]);
+        }
+        
+        
+
+        String newString =  "";
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            char lowercase = Character.toLowerCase(c);
+            if (lowercase == 'a' || lowercase == 'e' || lowercase == 'i' || lowercase == 'o' || lowercase == 'u') {
+                
+                if (freq[0] != 0) {
+                    newString += 'A';
+                    freq[0]--;
+                }
+                else if (freq[1] != 0) {
+                    newString += 'E';
+                    freq[1]--;
+                }
+                else if (freq[2] != 0) {
+                    newString += 'I';
+                    freq[2]--;
+                }
+                else if (freq[3] != 0) {
+                    newString += 'O';
+                    freq[3]--;
+                }
+                else if (freq[4] != 0) {
+                    newString += 'U';
+                    freq[4]--;
+                }
+                else if (freq[5] != 0) {
+                    newString += 'a';
+                    freq[5]--;
+                }
+                else if (freq[6] != 0) {
+                    newString += 'e';
+                    freq[6]--;
+                }
+                else if (freq[7] != 0) {
+                    newString += 'i';
+                    freq[7]--;
+                }
+                else if (freq[8] != 0) {
+                    newString += 'o';
+                    freq[8]--;
+                }
+                else {
+                    newString += 'u';
+                }
+            }
+            else {
+                newString += c;
+            }
+        }
+
+        return newString;
+    }
+}
