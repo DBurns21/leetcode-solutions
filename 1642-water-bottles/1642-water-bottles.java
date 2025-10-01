b4369560c4cdf9135553a401576fd60dfa7c7d6d
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-10-01 18:07:47 -0400

    Time: 10 ms (0.14%), Space: 41 MB (6.23%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int bottleExchange(int numBottles, int numExchange, int leftover) {
+        System.out.println("Current bottles: " + numBottles + "\nCurrent leftover: " + leftover);
+        if (numBottles + leftover < numExchange) {
+            return numBottles;
+        }
+
+        int exchanges = (numBottles + leftover) / numExchange;
+        System.out.println("Exchanges: " + exchanges +"\n");
+
+        return numBottles + bottleExchange(exchanges, numExchange, numBottles-(exchanges*numExchange) + leftover);
+    }
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        return bottleExchange(numBottles, numExchange, 0);
+    }
+}
