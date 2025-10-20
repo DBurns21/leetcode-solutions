379efb34494596c80a5230ae86d653da22fcd052
Dylan Burns <167133467+DBurns21@users.noreply.github.com>
2025-10-20 08:43:48 -0400

    Time: 0 ms (100%), Space: 42.2 MB (99.77%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -2,7 +2,10 @@ class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int x = 0;
         for (String operation : operations) {
-            if (operation.equals("X++") || operation.equals("++X")) {
+            //since this problem was very easy the only way to make it "harder" was find a way to optimize it which there is not a lot of room to do so
+            //but instead of checking if the whole string is equals you only need to check at most two character because either the first or 
+            //second character will tell you if you are adding or subtracting
+            if (operation.contains("+")) {
                 x++;
             }
             else {
